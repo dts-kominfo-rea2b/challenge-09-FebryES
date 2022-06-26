3ae3be4fc62e9bf11d99e15acbed4c0d81b6c9c1
FebryES <febry.ermansyah86@gmail.com>
2022-06-26 18:22:03 +0700

    tugas9

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,18 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+        <div className='card'>
+            <img src={props.data.photo} alt={props.data.name}/>
+            <div className='container'>
+                <h1>{props.data.name}</h1>
+                <h4>{props.data.phone}</h4>
+                <h4>{props.data.email}</h4>
+            </div>    
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css/';
 
 const Header = () => {
     return (
-        <></>
+        <div className='header'>
+            <h1>Call a Friend</h1>
+            <h5>Your friendly contact app</h5>
+            <div className='dot'>
+                <span>.....................</span>
+            </div>
+        </div>
     )
 }
 
